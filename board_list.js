b3f576175e8e599c7e3191a8f791b9a252cbb325
choisooyoung-dev <sooyoung.choi.dev@gmail.com>
2023-10-04 23:20:17 +0900

    feat: 게시글 제목 줄임말 css로 기능 구현 및 제목 눌렀을때 board_view.html로 이동

--- board_list.js
+++
@@ -1,5 +0,0 @@
-// 게시판 제목 특정 길이 넘으면 줄여주기
-let str = document.querySelector(".tdTitle").innerHTML;
-let cutStr = str.substring(0, 60) + " ...";
-
-document.querySelector(".tdTitle").innerHTML = cutStr;
