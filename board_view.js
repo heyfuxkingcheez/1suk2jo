a35da49676b01893e6cd5fad235f5d873167863f
eunji624 <allrkatk@naver.com>
2023-10-05 15:17:09 +0900

    [fix]: 커밋

--- board_view.js
+++ board_view.js
@@ -9,23 +9,33 @@ import {doc, updateDoc, deleteField, deleteDoc } from "https://www.gstatic.com/f
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //값 입력
+  apiKey: "AIzaSyBHruLjIXeoszzqiT2HSWT6nsIyKOEbeRU",
+  authDomain: "sparta-e533a.firebaseapp.com",
+  projectId: "sparta-e533a",
+  storageBucket: "sparta-e533a.appspot.com",
+  messagingSenderId: "176323692514",
+  appId: "1:176323692514:web:bf9dc31cafca3ffbb29bbb",
+  measurementId: "G-H5C27X8E9M"
 };
 
 // Firebase 인스턴스 초기화
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+let docs = await getDocs(collection(db, "board"));
+docs.forEach((doc) => {
+    let row = doc.data();
+    console.log(row);
+}); 
 
 
-
-//데이터 삭제.
-$('#delete').click(async function(e){
-  e.preventDefault();
-  console.log('gt');
-  //삭제 예시
-  // const desertRef = doc(db, [컬렉션명], [도큐멘트명], [하위컬렉션명], [삭제할 도큐멘트명]);
-  // await deleteDoc(desertRef);
-  let dodo = doc(db, 'eatJoo', 'one');
-  await deleteDoc(dodo);
-})
+// //데이터 삭제.
+// $('#delete').click(async function(e){
+//   e.preventDefault();
+//   console.log('gt');
+//   //삭제 예시
+//   // const desertRef = doc(db, [컬렉션명], [도큐멘트명], [하위컬렉션명], [삭제할 도큐멘트명]);
+//   // await deleteDoc(desertRef);
+//   let dodo = doc(db, 'eatJoo', 'one');
+//   await deleteDoc(dodo);
+// })
--- board_write.js
+++ board_write.js
@@ -10,7 +10,13 @@ import {doc, setDoc, deleteDoc} from "https://www.gstatic.com/firebasejs/9.22.0/
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+  apiKey: "AIzaSyBHruLjIXeoszzqiT2HSWT6nsIyKOEbeRU",
+  authDomain: "sparta-e533a.firebaseapp.com",
+  projectId: "sparta-e533a",
+  storageBucket: "sparta-e533a.appspot.com",
+  messagingSenderId: "176323692514",
+  appId: "1:176323692514:web:bf9dc31cafca3ffbb29bbb",
+  measurementId: "G-H5C27X8E9M"
 };
 
 // Firebase 인스턴스 초기화
@@ -24,7 +30,7 @@ $('#writeFrm').submit(async function(e){
   let now = new Date();
   
   let year = now.getFullYear();
-  let month = now.getMonth();
+  let month = now.getMonth() + 1;
   let date = now.getDate();
   let hours = now.getHours();
   let minutes = now.getMinutes();
@@ -33,18 +39,4 @@ $('#writeFrm').submit(async function(e){
   console.log(when);
 
   let writeTitle = $('#writeTitle').val();
-  let writeText = $('#writeText').val();
-  let writeName = $('#writeName').val();
-
-  console.log(writeTitle, writeName, writeText);
-  let docs = {
-    'writeTitle' : writeTitle,
-    'writeText' : writeText,
-    'writeName' : writeName,
-    'when' : when
-  }
-  console.log(docs)
-  await setDoc(doc(db, "board", '5'), docs);
-  alert("저장 완료!");
-  window.location.reload()
-});
+})
