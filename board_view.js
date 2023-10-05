1e752b63e61efde8df6511b95bfa139df7c84ddd
eunji624 <allrkatk@naver.com>
2023-10-05 20:29:44 +0900

    [feat]: view.html 데이터 가져오기

--- board_view.js
+++ board_view.js
@@ -15,31 +15,18 @@ import {
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyBv1pzj-eVAsCap6_XVd3WpTydkWuEsZOY",
+  authDomain: "ejoo-a1fd7.firebaseapp.com",
+  projectId: "ejoo-a1fd7",
+  storageBucket: "ejoo-a1fd7.appspot.com",
+  messagingSenderId: "982632789909",
+  appId: "1:982632789909:web:40149b8fa66ce19b1c289c"
 };
 // Firebase 인스턴스 초기화
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-
-let docs = await getDocs(collection(db, "board"));
-docs.forEach((doc) => {
-  let row = doc.data();
-  console.log(row);
-});
-
-// //데이터 삭제.
-// $('#delete').click(async function(e){
-//   e.preventDefault();
-//   console.log('gt');
-//   //삭제 예시
-//   // const desertRef = doc(db, [컬렉션명], [도큐멘트명], [하위컬렉션명], [삭제할 도큐멘트명]);
-//   // await deleteDoc(desertRef);
-//   let dodo = doc(db, 'eatJoo', 'one');
-//   await deleteDoc(dodo);
-// })
-
-
+//데이터 삭제
 $("#delete").click(async function (e) {
   e.preventDefault();
 
@@ -51,6 +38,32 @@ $("#delete").click(async function (e) {
 
   // let dodo = doc(db, 'eatJoo', '5');
   // await deleteDoc(dodo);
-  window.location.reload();
+  window.location.href = './board_list.html';
 });
 
+//데이터 보여주기
+let docs = await getDocs(collection(db, "board"));
+docs.forEach((doc) => {
+  let row = doc.data();
+  let writeTitle = row["writeTitle"];
+  let writeName = row["writeName"];
+  let when = row["when"];
+  let writeText = row['writeText'];
+  console.log(writeTitle, writeText, writeName, when)
+  console.log(docs);
+  let append_html = `
+    <div id="subject">
+      <span>제목 : ${writeTitle}</span>
+    </div>
+    <div id="wirter">
+      <span>작성자 : ${writeName}</span>  
+    </div>
+    <div id="date">
+      <span>작성일 : ${when}</span>  
+    </div>
+    <div id="content">
+      <span>내용 : ${writeText}</span>  
+    </div>`
+
+  $("#listCard").append(append_html);
+});
