aa7e71d5726edbb194e5ffebb24b4a2d7e527942
Your Name <you@example.com>
2023-10-05 14:51:37 +0900

    fix. addDocs로 고유아디값 생성

--- board_write.js
+++ board_write.js
@@ -10,7 +10,13 @@ import {doc, setDoc, deleteDoc} from "https://www.gstatic.com/firebasejs/9.22.0/
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+  apiKey: "AIzaSyBHruLjIXeoszzqiT2HSWT6nsIyKOEbeRU",
+  authDomain: "sparta-e533a.firebaseapp.com",
+  projectId: "sparta-e533a",
+  storageBucket: "sparta-e533a.appspot.com",
+  messagingSenderId: "176323692514",
+  appId: "1:176323692514:web:bf9dc31cafca3ffbb29bbb",
+  measurementId: "G-H5C27X8E9M"
 };
 
 // Firebase 인스턴스 초기화
@@ -42,9 +48,9 @@ $('#writeFrm').submit(async function(e){
     'writeText' : writeText,
     'writeName' : writeName,
     'when' : when
-  }
+  };
   console.log(docs)
-  await setDoc(doc(db, "board", '5'), docs);
+  await addDoc(collection(db, "board"), docs);
   alert("저장 완료!");
   window.location.reload()
 });
