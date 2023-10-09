d25ce44d086dfe9bc265fc141b2d5431b18cdc52
Your Name <you@example.com>
2023-10-09 22:08:30 +0900

    [fix] 댓글비번 코드수정

--- board_view.js
+++ board_view.js
@@ -207,17 +207,39 @@ $("#commentBtn").click(async function (e) {
 
 // 댓글 비밀번호 일치 확인
 $(function () {
+  // $("#pwd1").keyup(function () {
+  //   $("#chkNoticeCo").html("");
+  // });
+
   $("#pwd1").keyup(function () {
-    $("#chkNotice").html("");
+    if ($("#pwd2").length <= 0) {
+      $("#chkNoticeCo").css("diplay", "none")
+    }
+
+    else if ($("#pwd1").val() == $("#pwd2").val()) {
+      $("#chkNoticeCo").html("비밀번호 일치함<br><br>");
+      $("#chkNoticeCo")
+      .css("display", "block")
+      .css("color", "blue")
+    } 
   });
 
+// 첫번재 비번 입력 값이 없을 때, 비번 알림창 안보이게
   $("#pwd2").keyup(function () {
-    if ($("#pwd1").val() != $("#pwd2").val()) {
-      $("#chkNotice").html("비밀번호 일치하지 않음<br><br>");
-      $("#chkNotice").attr("color", "#f82a2aa3");
+    if ($("#pwd1").length <= 0) {
+      $("#chkNoticeCo").css("diplay", "none")
+    }
+
+    else if ($("#pwd1").val() != $("#pwd2").val()) {
+      $("#chkNoticeCo").html("비밀번호 일치하지 않음<br><br>");
+      $("#chkNoticeCo")
+      .css("display", "block")
+      .css("color", "red")
     } else {
-      $("#chkNotice").html("비밀번호 일치함<br><br>");
-      $("#chkNotice").attr("color", "#199894b3");
+      $("#chkNoticeCo").html("비밀번호 일치함<br><br>");
+      $("#chkNoticeCo")
+      .css("display", "block")
+      .css("color", "blue")
     }
   });
 });
